e8d170de64cdd88996f706f6850da96d50ea4ee8
ViliusKe <kerysvilius@gmail.com>
2025-07-03 22:29:50 +0300

    created new project

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors()); //add real URL to { origin: 'http://your-url.com' }
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_DB_CONNECTION)
+  .then(console.log("Connected to DB!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+// app.use("/", router);
+
+app.use((_req, res) => {
+  return res.status(404).json({
+    message: "This endpoint does not exist",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App started on port ${process.env.PORT}`);
+});
